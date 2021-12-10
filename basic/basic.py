d35a49f2794bf46b3bb3e42be00a5a55d62dd2b2
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2021-12-10 17:34:36 +0530

    Learn about basic python

--- basic/basic.py
+++ basic/basic.py
@@ -1,1 +1,26 @@
-print("hi")
+# pros of Dynamic Typing in python
+#  Very easy to work with
+# Faster developement time
+
+# cons 
+# ! May result in bugs fro unexpected data types !
+# ! You need to be aware of type()
+
+a = 10
+a = a + a
+print("Value after adding a + a \n")
+print(a)
+
+# Simple string printing
+myname = "Sourabh Semalty"
+
+print("0 index is \t",myname[0])
+# Simple splice method with string
+# till 3 print
+print(myname[:3])
+
+# 1 to 3 print
+print(myname[1:3])
+
+# jump print
+print(myname[::2])
